e20dac83267714e0db89979a63011a2a12272a54
Admin <anstdor@gmail.com>
2022-08-09 20:23:07 +0300

    HW is done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,45 @@
  public class Main {
-        public static void main(String[] args){
-            // Задание 1
-            int i = 1;
-            while (i <= 10) {
-                System.out.println(i++);
-            }
-            {
-                System.out.println();
-            }
-            int a = 8;
-            for (a = 8; a > 10; --a) {
-                System.out.println(--a);
-            }
-    }
-}
+     public static void main(String[] args) {
+         // Задание 1
+         int i = 1;
+         while (i <= 10) {
+             System.out.print(i++);
+         }
+         {
+             System.out.println();
+         }
+         int a = 10;
+         for (a = 10; a >= 1; a--) {
+             System.out.print(a);
+         }
+
+         // Задание 2
+
+         {
+             System.out.println();
+         }
+
+         int dateFriday = 5;
+         for (dateFriday = 5; dateFriday <= 31; dateFriday += 7) {
+             System.out.println("Сегодня " + dateFriday + " -е число месяца. Необходимо подготовить отчет");
+
+         }
+
+         // Задание 3
+
+         {
+             System.out.println();
+         }
+
+         int yearGap = 79;
+         int startDate = 1822;
+         int finalDate = 2122;
+
+         for (startDate = 1822; startDate <= 2122; startDate++) {
+             if (startDate % 79 == 0) {
+                 System.out.println(startDate);
+             }
+
+         }
+     }
+ }
