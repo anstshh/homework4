f2a4ab6de040afe533f606e2d855b23a469e3e3d
Admin <anstdor@gmail.com>
2022-08-09 10:37:52 +0300

    Initial commit 2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+ public class Main {
+        public static void main(String[] args){
+            // Задание 1
+            int i = 1;
+            while (i <= 10) {
+                System.out.println(i++);
+            }
+            {
+                System.out.println();
+            }
+            int a = 8;
+            for (a = 8; a > 10; --a) {
+                System.out.println(--a);
+            }
+    }
+}
